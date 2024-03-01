7bd44f166ef764213083d7c6e7bb1af77e4ada7a
Steven Heinze <steven.heinze@johnstonhowse.com>
2024-03-01 00:14:04 -0600

    Finished 4

---
+++ 4/NOTES.md
@@ -0,0 +1,2 @@
+
+I got held up here trying to dynamically set the variable name based on the ip address. I did try a few different ways but to no avail. After figuring this out, then the only thing left would be to sort the values in descending order. 
---
+++ 4/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+set -e 
+
+unique_ips=()
+while read -r line; do
+    ip=$(echo $line | cut -d ' ' -f 2)
+    if [[ $(echo ${unique_ips[@]} | grep -w $ip) ]]; then 
+        echo "Already exists" 
+    else 
+        echo "Doesnt exist" 
+        unique_ips+=($ip)
+        trimmed_ip=$(echo $ip | tr -d '.')
+        # TODO: Need to dynamically set the variable value 
+        if [ -z ${ip} ]; then 
+            declare "$trimmed_ip=0"
+        else 
+            declare "$trimmed_ip=$trimmed_ip + 1"
+        fi 
+    fi
+done < logfile.txt 
+
+# TODO: Figure out why bash isnt reading the last line of the logfile. 
+# (Was able to bandage this by adding a new blank line at the end) 
+
+for ip in "${unique_ips[@]}"; 
+do 
+    echo ""
+    echo $ip 
+done 
